3cd67b2a0610a2bd18ce16b711ed01dcd2207bf0
sangpok <48979587+sangpok@users.noreply.github.com>
2023-05-08 22:10:22 +0900

    fixed: 주석 제거

--- Server/controllers/comments.js
+++ Server/controllers/comments.js
@@ -8,15 +8,9 @@ const getAllComments = async (req, res) => {
     res.status(500).json({ msg: error });
   }
 };
-//   "name": "김주현",
-//   "date": "2023/04/28 17:44",
-//   "comment": {
-//     "type": "text",
-//     "content": "자~ 뒤졌습니다~"
-//   },
+
 const createComment = async (req, res) => {
   try {
-    // console.log(req);
     const comment = await Comment.create(req.body);
     res.status(201).json({ comment });
   } catch (error) {
@@ -40,7 +34,6 @@ const updateComment = (req, res) => {
 const deleteComment = async (req, res) => {
   try {
     const comments = await Comment.findOneAndDelete({ _id: req.params.id });
-    console.log(comments);
     res.status(200).json({ comments });
   } catch (error) {
     res.status(500).json({ msg: error });
