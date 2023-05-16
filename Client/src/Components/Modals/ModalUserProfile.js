72560505c53712c745dc0153894400ab759360e9
sangpok <48979587+sangpok@users.noreply.github.com>
2023-05-16 16:45:44 +0900

    fix: 버그

--- Client/src/Components/Modals/ModalUserProfile.js
+++ Client/src/Components/Modals/ModalUserProfile.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { MODAL_ACTION_TYPE, useModal } from '../../Context/ModalContext';
 import Modal from '../Modal';
 
+import { StyledModalUserProfile } from './ModalUserProfile.styled';
+
 const ModalUserProfile = ({ title, datas }) => {
   const { modalDispatch } = useModal();
 
@@ -13,7 +15,9 @@ const ModalUserProfile = ({ title, datas }) => {
     <>
       <Modal modalTitle={datas.userName + title} onSubmit={handleSubmit}>
         {/* <UserProfile userProfile={datas.profile} /> */}
-        <img src={datas.userProfile} alt="" />
+        <StyledModalUserProfile.ImageWrapper>
+          <StyledModalUserProfile.Image src={datas.userProfile} alt="" />
+        </StyledModalUserProfile.ImageWrapper>
       </Modal>
     </>
   );
